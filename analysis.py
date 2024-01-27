0341166de43855ff1217fb6f97d5af7e95bf160b
t20cs009_lab <t20cs009@gmail.com>
2024-01-27 20:02:31 +0900

    [add]
    かっつーが登場するよ

--- analysis.py
+++ analysis.py
@@ -29,6 +29,10 @@ class FERApp:
         self.frames_since_last_update = 0  # 最後の更新からのフレーム数
         self.last_update_time = time.time()
         
+        self.threshold_angry = 0.3  # この値を適切な閾値に設定
+        self.second_window = None
+        self.second_window_label = None
+        
         self.window_id = window_id
 
         # Dictionary to map emotions to image paths
@@ -53,50 +57,78 @@ class FERApp:
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             result = self.fer.detect_emotions(frame)
+            
+            if result:
+                for face in result:
+                    emotion = face['emotions']
+                    self.emotion_history.append(emotion)
+                    self.frames_since_last_update += 1
 
-            for face in result:
-                emotion = face['emotions']
-                if self.window_id == 1:
-                    emotion['happy'] *= 1.2
-                    emotion['sad'] *= 0.5
-                    emotion['angry'] *= 0.5
-                    emotion['fear'] *= 0.5
+                current_time = time.time()
+                elapsed_time = current_time - self.last_update_time
 
-                self.emotion_history.append(emotion)
-                self.frames_since_last_update += 1
+                if elapsed_time >= 1:  # 1秒ごとに処理
+                    # 平均感情の計算
+                    if self.emotion_history:  # リストが空でない場合に処理を行う
+                        average_emotion = {emotion: sum(data[emotion] for data in self.emotion_history) / len(self.emotion_history) for emotion in result[0]['emotions']}
+                         # 一番大きな平均値を持つ感情を取得
+                        dominant_emotion = max(average_emotion, key=average_emotion.get)
+                        
+                        print(average_emotion['angry'])
+                        if average_emotion['angry'] > self.threshold_angry:
+                            print('hello')
+                            self.show_second_window()
+                        elif  self.second_window is not None:
+                            self.second_window.destroy()
+                            self.second_window = None
+                            
+                    else:
+                         #Set neutral as default
+                        dominant_emotion = 'neutral'
+                
+                    # Update the label with the dominant emotion
+                    self.label_emotion.config(text=f"Dominant emotion: {dominant_emotion.capitalize()}")
 
-            current_time = time.time()
-            elapsed_time = current_time - self.last_update_time
+                    # Display an image based on the detected emotion
+                    if dominant_emotion in self.emotion_images:
+                        img_path = self.emotion_images[dominant_emotion]
+                        img = Image.open(img_path)
+                        img = ImageTk.PhotoImage(img)
+                        self.image_label.config(image=img)
+                        self.image_label.image = img  # Keep a reference to avoid garbage collection
 
-            if elapsed_time >= 1:  # 1秒ごとに処理
-                # 平均感情の計算
-                if self.emotion_history:  # リストが空でない場合に処理を行う
-                    average_emotion = {emotion: sum(data[emotion] for data in self.emotion_history) / len(self.emotion_history) for emotion in result[0]['emotions']}
-                     # 一番大きな平均値を持つ感情を取得
-                    dominant_emotion = max(average_emotion, key=average_emotion.get)
-                else:
-                     # 一番大きな平均値を持つ感情を取得
-                    dominant_emotion = 'neutral'
-
-               
-
-                # Update the label with the dominant emotion
-                self.label_emotion.config(text=f"Dominant emotion: {dominant_emotion.capitalize()}")
-
-                # Display an image based on the detected emotion
-                if dominant_emotion in self.emotion_images:
-                    img_path = self.emotion_images[dominant_emotion]
-                    img = Image.open(img_path)
-                    img = ImageTk.PhotoImage(img)
-                    self.image_label.config(image=img)
-                    self.image_label.image = img  # Keep a reference to avoid garbage collection
-
-                # リストをクリアして次のデータ収集の準備
-                self.emotion_history.clear()
-                self.frames_since_last_update = 0
-                self.last_update_time = time.time()
+                    # リストをクリアして次のデータ収集の準備
+                    self.emotion_history.clear()
+                    self.frames_since_last_update = 0
+                    self.last_update_time = time.time()
 
         self.root.after(100, self.update)
+        
+    def show_second_window(self):
+        # 別のウィンドウが既に存在する場合は閉じる
+        if self.second_window is not None:
+            self.second_window.destroy()
+
+        # 別のウィンドウを作成
+        self.second_window = tk.Toplevel(self.root)
+        self.second_window.title("Angry Image")
+
+        # 別のウィンドウに画像を表示
+        img_path = 'img/kattu2.jpg'  # ここに別の画像のパスを設定
+        img = Image.open(img_path)
+        img = ImageTk.PhotoImage(img)
+        self.second_window_label = tk.Label(self.second_window, image=img)
+        self.second_window_label.image = img
+        self.second_window_label.pack()
+
+        # 別のウィンドウを閉じるためのボタン
+        close_button = tk.Button(self.second_window, text="Close", command=self.close_second_window)
+        close_button.pack()
+
+    def close_second_window(self):
+        if self.second_window is not None:
+            self.second_window.destroy()
+            self.second_window = None
         
     def exit_app(self):
         self.root.destroy()
--- analysis_gui.py
+++ analysis_gui.py
@@ -45,44 +45,45 @@ class FERApp:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             result = self.fer.detect_emotions(frame)
 
-            for face in result:
-                emotion = face['emotions']
+            if result:
+                for face in result:
+                    emotion = face['emotions']
+
+                    # emotion['happy'] *= 1.2
+                    # emotion['sad'] *= 0.5
+                    # emotion['angry'] *= 0.5
+
+                    self.emotion_history.append(emotion)
+                    self.frames_since_last_update += 1
+
+                current_time = time.time()
+                elapsed_time = current_time - self.last_update_time
+
+                if elapsed_time >= 1:  # 1秒ごとに処理
+                    # 平均感情の計算
+                    if self.emotion_history:  # リストが空でない場合に処理を行う
+                        average_emotion = {emotion: sum(data[emotion] for data in self.emotion_history) / len(self.emotion_history) for emotion in result[0]['emotions']}
+                         # 一番大きな平均値を持つ感情を取得
+                        dominant_emotion = max(average_emotion, key=average_emotion.get)
+                    else:
+                         #Set neutral as default
+                        dominant_emotion = 'neutral'
                 
-                emotion['happy'] *= 1.2
-                emotion['sad'] *= 0.5
-                emotion['angry'] *= 0.5
-                
-                self.emotion_history.append(emotion)
-                self.frames_since_last_update += 1
+                    # Update the label with the dominant emotion
+                    self.label_emotion.config(text=f"Dominant emotion: {dominant_emotion.capitalize()}")
 
-            current_time = time.time()
-            elapsed_time = current_time - self.last_update_time
+                    # Display an image based on the detected emotion
+                    if dominant_emotion in self.emotion_images:
+                        img_path = self.emotion_images[dominant_emotion]
+                        img = Image.open(img_path)
+                        img = ImageTk.PhotoImage(img)
+                        self.image_label.config(image=img)
+                        self.image_label.image = img  # Keep a reference to avoid garbage collection
 
-            if elapsed_time >= 1:  # 1秒ごとに処理
-                # 平均感情の計算
-                if self.emotion_history:  # リストが空でない場合に処理を行う
-                    average_emotion = {emotion: sum(data[emotion] for data in self.emotion_history) / len(self.emotion_history) for emotion in result[0]['emotions']}
-                     # 一番大きな平均値を持つ感情を取得
-                    dominant_emotion = max(average_emotion, key=average_emotion.get)
-                else:
-                     #Set default as neutral
-                    dominant_emotion = 'neutral'
-                
-                # Update the label with the dominant emotion
-                self.label_emotion.config(text=f"Dominant emotion: {dominant_emotion.capitalize()}")
-
-                # Display an image based on the detected emotion
-                if dominant_emotion in self.emotion_images:
-                    img_path = self.emotion_images[dominant_emotion]
-                    img = Image.open(img_path)
-                    img = ImageTk.PhotoImage(img)
-                    self.image_label.config(image=img)
-                    self.image_label.image = img  # Keep a reference to avoid garbage collection
-
-                # リストをクリアして次のデータ収集の準備
-                self.emotion_history.clear()
-                self.frames_since_last_update = 0
-                self.last_update_time = time.time()
+                    # リストをクリアして次のデータ収集の準備
+                    self.emotion_history.clear()
+                    self.frames_since_last_update = 0
+                    self.last_update_time = time.time()
 
         self.root.after(100, self.update)
 
